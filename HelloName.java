7a5865680423547eed206177a253c71ae8f2612f
febin-04 <febinnoble2028@cs.sjcetpalai.ac.in>
2025-07-11 00:46:29 +0530

    Update HelloName.java

--- HelloName.java
+++ HelloName.java
@@ -7,6 +7,8 @@ public class HelloName {
 
         System.out.println("What is your name?");
         line = scanner.nextLine();
+         System.out.println("Hello "+line+"!");
+        
 
         // Put the correct code here!
     }
